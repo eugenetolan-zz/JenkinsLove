b8243a6f87c9bd097d308601f279079049f96cfb
eugenetolan <Eugene.Tolan@bskyb.com>
2014-11-17 10:31:55 +0000

    Delete puppy.pp

--- puppy.pp
+++
@@ -1,7 +0,0 @@
-class { 'apache':
-  template => 'example42/apache/httpd.conf.erb',
-}
-apache::vhost { 'mysite':
-  docroot  => '/path/to/docroot',
-  template => 'example42/apache/vhost/mysite.com.erb',
-}
